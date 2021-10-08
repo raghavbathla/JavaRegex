37c56f006597d84304e834ca2376fe3d18df36fb
Raghav <bathla.raghav5@gmail.com>
2021-10-08 08:31:51 +0530

    [Raghav] method to verify Last name added

--- src/userRegestration.java
+++ src/userRegestration.java
@@ -1,8 +1,10 @@
 public class userRegestration {
-    public static void main(String[] args)
-    {
-UserRegistration userRegistration = new UserRegistration();
-System.out.println("Enter the first name");
-userRegistration.validName();
+    public static void main(String[] args) {
+        UserRegistration userRegistration = new UserRegistration();
+        System.out.println("Enter the first name");
+        String fName = userRegistration.validName();
+        System.out.println("Enter the last name");
+        String lName = userRegistration.validName();
+
     }
 }
